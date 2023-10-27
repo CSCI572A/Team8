ace1f4000cada7c6531f6dc04c4b0b00e946e41a
parisfloyd <parisfloyd@mines.edu>
2023-10-27 16:58:44 -0600

    Got the SSID and MAC address to print for the current network. Did research on different network analysis packages and imported them.

--- WiFiBuddy.py
+++ WiFiBuddy.py
@@ -1,2 +1,31 @@
 # Paris Floyd, Abhaya Shrestha
 # Final project: WiFiBuddy - Evil Twin Attack Mitigation
+
+import subprocess
+import pyshark
+import os
+import sys
+
+import subprocess
+
+def get_ssid_windows():
+    result = subprocess.check_output(["netsh", "wlan", "show", "interfaces"])
+    result = result.decode("utf-8").split("\n")
+    for line in result:
+        if "SSID" in line:
+            return line.strip().split(":")[1].strip()
+
+import subprocess
+
+def get_mac_address_windows():
+    result = subprocess.check_output(["getmac"]).decode("utf-8")
+    lines = result.splitlines()
+    if len(lines) >= 3:
+        # The MAC address is usually in the second line, but you can adjust the index if needed.
+        return lines[3].split()[0]
+
+
+ssid = get_ssid_windows()
+print("Current SSID:", ssid)
+mac_address = get_mac_address_windows()
+print("MAC Address:", mac_address)
